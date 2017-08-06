656de372c475540b42c94cdafff764aaa61870de
Gabriel Lidenor <gabrielslack@gmail.com>
2017-08-06 09:42:20 -0300

    the todo list model

---
+++ api/models/todoListModel.js
@@ -0,0 +1,24 @@
+'use strict';
+var mongose = require('mongose');
+var Schema = mongose.Schema;
+
+var TaskSchema = new Schema({
+
+  name: {
+    type: String,
+    Require: 'Kindly enter the name of the task'
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'ongoing', 'completed']
+    }],
+    
+  default: ['pending']
+  }
+})
